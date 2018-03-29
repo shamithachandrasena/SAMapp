36ff9a9b91d27f88c0effe2944a70492773a3927
ShamithaChandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-03-29 12:29:52 +0530

    created styles and urls for nav.component

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,12 @@
+/**
+ * New typescript file
+ */
+import { Component } from '@angular/core';
+@Component({
+  selector:'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class navComponent implements OnInit {
+  title = 'app';
+}
