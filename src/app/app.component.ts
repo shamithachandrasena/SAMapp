77f1875efd072546bab4c668c9cc7f2f1575a2c0
ShamithaChandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-03-29 12:02:01 +0530

    changed app component to paragraph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<p>I love you</p>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
