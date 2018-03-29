803efcdd256d4855acc3e4c7cac6527c09b0303f
ShamithaChandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-03-29 13:31:16 +0530

    defined the constructor of Friend model

--- src/app/Friend.model.ts
+++ src/app/Friend.model.ts
@@ -3,4 +3,10 @@ export class Friend{
   public age: number;
   public email: string;
   public image: string;
+  
+  constructor(name, age, email, image){
+    this.name = name;
+    this.age = age;
+    this.email = image;
+    }
 }
