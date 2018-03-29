f31f50ce3def5b8de802a101090b7cd9ae1f31cf
ShamithaChandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-03-29 16:29:26 +0530

    created frind in friendlist and cleared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FriendListComponent } from './friend-list/friend-list.component';
+import { FriendComponent } from './friend-list/friend/friend.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FriendListComponent
+    FriendListComponent,
+    FriendComponent
   ],
   imports: [
     BrowserModule
