ff0f1694e37144bce4303096f8a99ea939555cfd
ShamithaChandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-03-29 13:56:35 +0530

    template binded to get friend model data-name

--- src/app/Friend.model.ts
+++ src/app/Friend.model.ts
@@ -1,12 +1,9 @@
 export class Friend{
-  public name: string;
-  public age: number;
-  public email: string;
-  public image: string;
   
-  constructor(name, age, email, image){
-    this.name = name;
-    this.age = age;
-    this.email = image;
+  constructor(public name: string,
+              public age: number,
+              public email: string,
+              public image: string){
+   
     }
 }
--- src/app/friend-list/friend-list.component.ts
+++ src/app/friend-list/friend-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Friend } from '../Friend.model';
 
 @Component({
   selector: 'app-friend-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friend-list.component.css']
 })
 export class FriendListComponent implements OnInit {
+
+  friend : Friend = new Friend('shamitha', 23, 'shamithachandrasena@gmai.com', 'test');
 
   constructor() { }
 
