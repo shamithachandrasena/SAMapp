8bd5ab242f8a9661c5f8f0d0ccc1c41b977906a0
ShamithaChandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-04-01 02:42:23 +0530

    added name component and location component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FriendListComponent } from './friend-list/friend-list.component';
 import { FriendComponent } from './friend-list/friend/friend.component';
+import { NameComponent } from './name/name.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FriendListComponent,
-    FriendComponent
+    FriendComponent,
+    NameComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule
